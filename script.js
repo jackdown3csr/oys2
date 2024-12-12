de3d355097f791ead992d83ebf91fadacc1070f1
jackdown3csr <dolejsjan83@gmail.com>
2024-12-12 18:43:31 +0100

    initii

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    var password = prompt("Enter password to access Turbo downloader:");
+    if (password && password.toLowerCase() === "rev") {
+        document.getElementById('content').style.display = 'block';
+        // Create download links
+        var contentDiv = document.getElementById('content');
+        contentDiv.innerHTML = 
+            '<h2>Welcome to Turbo downloader</h2>' +
+            '<ul class="file-list">' +
+                '<li><a href="downloads/file1.txt" download>file1.txt</a></li>' +
+                '<li><a href="downloads/file2.zip" download>file2.zip</a></li>' +
+                '<li><a href="downloads/image.jpg" download>image.jpg</a></li>' +
+            '</ul>';
+    } else {
+        alert("Access denied.");
+    }
+};
